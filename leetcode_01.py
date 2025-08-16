6c93bfdf76a101f3e7eed49570d436b54510f2d1
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-08-16 14:49:35 +0600

    two sum

---
+++ leetcode_01.py
@@ -0,0 +1,14 @@
+nums = [2, 7, 11, 15]
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_index:
+                return [num_index[complement], i]
+            num_index[num] = i
+        return []
+
+sol = Solution()
+print(sol.twoSum(nums, 9))
