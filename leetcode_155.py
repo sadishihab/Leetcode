0ca6cc5278690fc93859dbb93a310e74d7ebece4
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-08-17 14:45:24 +0600

    Min Stack

---
+++ leetcode_155.py
@@ -0,0 +1,32 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = [float('inf')]
+
+    def push(self, val):
+        self.stack.append(val)
+        self.minStack.append(min(val, self.minStack[-1]))
+
+    def pop(self):
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.minStack[-1]
+
+minStack = MinStack()
+print(minStack.stack)
+minStack.push(-2)
+print(minStack.stack)
+minStack.push(0)
+print(minStack.stack)
+minStack.push(-3)
+print(minStack.stack)
+print(minStack.getMin()) # return -3
+print(minStack.pop())
+print(minStack.top())    # return 0
+print(minStack.getMin())  #return -2
