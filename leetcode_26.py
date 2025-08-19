18c2423854141c8582220a7b754515c127069d5a
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-08-19 12:04:21 +0600

    Remove Duplicates from Sorted Array

--- leetcode_26.py
+++ leetcode_26.py
@@ -1,10 +1,7 @@
 # Remove Duplicates from Sorted Array
-#Approach: Two-Pointer Technique
-# The problem can be solved efficiently using a two-pointer technique since the array is already sorted. it is used because:
-        #In-Place Modification: The problem requires modifying the array in-place without using extra space. The two-pointer method allows us to overwrite duplicate elements with unique ones.
-        #Efficient Traversal: Since the array is sorted, duplicates are adjacent. By comparing adjacent elements, we can identify unique elements in a single pass (O(n) time).
-        #Track Unique Elements: One pointer (k) tracks the position where the next unique element should be placed, while the other (i) iterates through the array to find unique elements.
-        #Simplicity and Order Preservation: The approach ensures unique elements are placed in the correct order (as they appear in the sorted array) with minimal operations.
+#Approach: Two-Pointers Technique
+# Time: O(n) → one pass through the array
+# Space: O(1) → in-place, no extra memory
 
 class Solution(object):
     def removeDuplicates(self, nums):
