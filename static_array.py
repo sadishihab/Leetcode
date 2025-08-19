22f6512b916fd542afab6280c0ab3ebd0ea0713b
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-08-19 12:15:49 +0600

    Simple static array

--- static_array.py
+++ static_array.py
@@ -1,7 +1,4 @@
-
-
-
-# Insert n into arr at the next open position. 
+# Insert n into arr at the next open position.
 # Length is the number of 'real' values in arr, and
 # capacity is the size (aka memory allocated for the fixed size array).
 def insertEnd(arr, n, length, capacity):
