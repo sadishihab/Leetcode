5bebe4243da25dc8feb965105c1e216e3a136f22
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-08-28 16:12:55 +0600

    Merge Two Sorted Lists

---
+++ leetcode_19_recursive.py
@@ -0,0 +1,18 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        # Base cased
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        # Recursive cases
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
