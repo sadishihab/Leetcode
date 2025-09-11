566848c6c727d8f7058d2bcfe5ddf19faff26bcc
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-09-11 18:59:13 +0600

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,28 @@
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+# Example
+print(merge_sort([5, 2, 3, 1, 4]))  # Output: [1, 2, 3, 4, 5]
