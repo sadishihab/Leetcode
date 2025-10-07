3ff3cd82cebc9aa1c453269b2ecfe9f490807431
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-10-07 18:14:27 +0600

    Design HashMap

--- leetcode_706.py
+++ leetcode_706.py
@@ -1,23 +1,16 @@
 class MyHashMap:
-    """
-    HashMap implementation using separate chaining (list of lists).
-    Each bucket stores key-value pairs as [key, value].
-    """
+    """ HashMap implementation using separate chaining (list of lists). Each bucket stores key-value pairs as [key, value]. """
 
     def __init__(self):
         self.capacity = 1000                    # Number of buckets
         self.buckets = [[] for _ in range(self.capacity)]
 
     def _hash(self, key):
-        """
-        Simple hash function to map key to bucket index.
-        """
+        """ Simple hash function to map key to bucket index. """
         return key % self.capacity
 
     def put(self, key, value):
-        """
-        Insert a key-value pair or update existing key.
-        """
+        """ Insert a key-value pair or update existing key. """
         index = self._hash(key)
         bucket = self.buckets[index]
 
@@ -31,9 +24,7 @@ class MyHashMap:
         bucket.append([key, value])
 
     def get(self, key):
-        """
-        Return value associated with key, or -1 if not found.
-        """
+        """ Return value associated with key, or -1 if not found. """
         index = self._hash(key)
         bucket = self.buckets[index]
 
@@ -44,9 +35,7 @@ class MyHashMap:
         return -1
 
     def remove(self, key):
-        """
-        Remove key from the map if it exists.
-        """
+        """ Remove key from the map if it exists. """
         index = self._hash(key)
         bucket = self.buckets[index]
 
