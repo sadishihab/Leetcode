ec3663f6e7957ced2d8c090155199815a128fbb7
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-11-15 12:45:51 +0600

    bit_manipulation

---
+++ bit_manipulation.py
@@ -0,0 +1,9 @@
+
+# Counting Bits
+def countBits(n):
+    count = 0
+    while n > 0:
+        if n & 1 == 1:
+            count += 1
+        n = n >> 1           # same as n // 2
+    return count
