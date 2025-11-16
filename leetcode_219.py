7a452db6f4957b1bce99695e080c508ba12e4040
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2025-11-16 19:55:54 +0600

    update

--- leetcode_219.py
+++ leetcode_219.py
@@ -4,19 +4,19 @@ from typing import List
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         window = set()
-        left = 0
+        L = 0
 
-        for right in range(len(nums)):
+        for R in range(len(nums)):
             # If the window size exceeds k, remove the leftmost element
-            if right - left > k:
-                window.remove(nums[left])
-                left += 1
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
 
             # If the number is already inside the window → duplicate found
-            if nums[right] in window:
+            if nums[R] in window:
                 return True
 
             # Add current number to window
-            window.add(nums[right])
+            window.add(nums[R])
 
         return False
