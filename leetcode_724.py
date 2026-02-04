7c11e527a12a160f0cc2718401b2703ea164a0e7
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2026-02-04 15:34:55 +0600

    Find Pivot Index

---
+++ leetcode_724.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        left_sum = 0
+
+        for i, num in enumerate(nums):
+            right_sum = total - left_sum - num
+
+            if left_sum == right_sum:
+                return i
+
+            left_sum += num
+
+        return -1
