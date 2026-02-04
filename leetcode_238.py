a02e5c9cb6e9d9613d7282497fb03e4d14624129
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2026-02-04 17:02:19 +0600

    Product of Array Except Self

---
+++ leetcode_238.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        # Step 1: Prefix products
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        # Step 2: Suffix products
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+
+        return answer
