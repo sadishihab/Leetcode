85df568b094a73c507a363f059a8dcae166a2540
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2026-03-11 17:08:36 +0600

    Ones and Zeroes

---
+++ leetcode_474.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findMaxForm(self, strs, m, n):
+
+        # convert strings to weights
+        items = []
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+            items.append((zeros, ones))
+
+        return knapsack(items, m, n)
+
+
+def knapsack(items, m, n):
+
+    dp = [[0]*(n+1) for _ in range(m+1)]
+
+    for zeros, ones in items:
+
+        for i in range(m, zeros-1, -1):
+            for j in range(n, ones-1, -1):
+
+                dp[i][j] = max(
+                    dp[i][j],
+                    1 + dp[i-zeros][j-ones]
+                )
+
+    return dp[m][n]
