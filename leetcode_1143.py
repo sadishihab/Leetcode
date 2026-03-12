b1aa2dfa1b5ff4d659617d3644c9ddba15c1b6e1
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2026-03-12 15:49:41 +0600

    Longest Common Subsequence

--- leetcode_1143.py
+++ leetcode_1143.py
@@ -1,15 +1,17 @@
-class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n, m = len(text1), len(text2)
-        prevRow = [0] * (m + 1)
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        # your dp function unchanged
+        def dp(s1, s2):
+            N, M = len(s1), len(s2)
+            dp = [[0] * (M+1) for _ in range(N+1)]
 
-        for i in range(n - 1, -1, -1):
-            curRow = [0] * (m + 1)
-            for j in range(m - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    curRow[j] = 1 + prevRow[j + 1]
-                else:
-                    curRow[j] = max(prevRow[j], curRow[j + 1])
-            prevRow = curRow
+            for i in range(N):
+                for j in range(M):
+                    if s1[i] == s2[j]:
+                        dp[i+1][j+1] = 1 + dp[i][j]
+                    else:
+                        dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
 
-        return prevRow[0]
+            return dp[N][M]
+
+        return dp(text1, text2)
