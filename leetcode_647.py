f27db1e23421195cfbab928715115ef9e74368ca
Md Shihabuddin Sadi <sadi.shihab@gmail.com>
2026-03-14 17:57:37 +0600

    Palindromic Substrings

---
+++ leetcode_647.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSubstrings(self, s):
+        def expand(l, r):
+            count = 0
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            return count
+
+        ans = 0
+
+        for i in range(len(s)):
+            ans += expand(i, i)
+            ans += expand(i, i + 1)
+
+        return ans
